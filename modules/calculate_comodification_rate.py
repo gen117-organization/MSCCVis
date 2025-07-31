211fbe58f448a958e29c5a799ac38d51396080cc
YukiOhta0519 <ohtayuki0519@gmail.com>
2025-07-31 13:00:11 +0900

    色々ミスってた4

--- modules/calculate_comodification_rate.py
+++ modules/calculate_comodification_rate.py
@@ -14,29 +14,29 @@ def analyze_repo(project):
     for language in languages:
         with open(project_root / "dest/csv" / name / f"{language}.csv", "r") as f:
             reader = csv.DictReader(f)
-        clonesets = {}
-        for row in reader:
-            clone_id = row["clone_id"]
-            if clone_id not in clonesets:
-                clonesets[clone_id] = []
-            clonesets[clone_id].append(row)
-        count = 0
-        comodification_count = 0
-        for clone_id, rows in clonesets.items():
-            modifications = {}
-            for row in rows:
-                for m in row["modification"]:
-                    if m["commit"] not in modifications:
-                        modifications[m["commit"]] = []
-                    modifications[m["commit"]].append(m["type"])
-            comodification = False
-            for commit, types in modifications.items():
-                if types.count("modified") >= 2:
-                    comodification = True
-            if comodification:
-                comodification_count += 1
-            count += 1
-        print(f"{name} {language} {comodification_count} / {count} = {comodification_count / count}")
+            clonesets = {}
+            for row in reader:
+                clone_id = row["clone_id"]
+                if clone_id not in clonesets:
+                    clonesets[clone_id] = []
+                clonesets[clone_id].append(row)
+            count = 0
+            comodification_count = 0
+            for clone_id, rows in clonesets.items():
+                modifications = {}
+                for row in rows:
+                    for m in row["modification"]:
+                        if m["commit"] not in modifications:
+                            modifications[m["commit"]] = []
+                        modifications[m["commit"]].append(m["type"])
+                comodification = False
+                for commit, types in modifications.items():
+                    if types.count("modified") >= 2:
+                        comodification = True
+                if comodification:
+                    comodification_count += 1
+                count += 1
+            print(f"{name} {language} {comodification_count} / {count} = {comodification_count / count}")
 
 if __name__ == "__main__":
     dataset_file = project_root / "dataset/selected_projects.json"
