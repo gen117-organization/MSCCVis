e0af83cc4556bbb9336d8b270048d28206bea063
YukiOhta0519 <ohtayuki0519@gmail.com>
2025-07-31 12:55:03 +0900

    同時修正率算出してみる．

---
+++ modules/calculate_comodification_rate.py
@@ -0,0 +1,51 @@
+import json
+import sys
+from pathlib import Path
+import csv
+
+project_root = Path(__file__).parent.parent
+sys.path.append(str(project_root))
+
+
+def analyze_repo(project):
+    project_root = Path(project)
+    name = project_root.name
+    language = project_root.parent.name
+    dest_dir = project_root / "dest/csv" / name
+    dest_dir.mkdir(parents=True, exist_ok=True)
+    with open(dest_dir / f"{language}.csv", "r") as f:
+        reader = csv.DictReader(f)
+        clonesets = {}
+        for row in reader:
+            clone_id = row["clone_id"]
+            if clone_id not in clonesets:
+                clonesets[clone_id] = []
+            clonesets[clone_id].append(row)
+        count = 0
+        comodification_count = 0
+        for clone_id, rows in clonesets.items():
+            modifications = {}
+            for row in rows:
+                for m in row["modification"]:
+                    if m["commit"] not in modifications:
+                        modifications[m["commit"]] = []
+                    modifications[m["commit"]].append(m["type"])
+            comodification = False
+            for commit, types in modifications.items():
+                if types.count("modified") >= 2:
+                    comodification = True
+            if comodification:
+                comodification_count += 1
+            count += 1
+        print(f"{name} {language} {comodification_count} / {count} = {comodification_count / count}")
+
+if __name__ == "__main__":
+    dataset_file = project_root / "dataset/selected_projects.json"
+    with open(dataset_file, "r") as f:
+        dataset = json.load(f)
+    outputs = []
+    for project in dataset:
+        url = project["URL"]
+        name = url.split("/")[-2] + "." + url.split("/")[-1]
+        workdir = project_root / "dest/projects" / name
+        analyze_repo(workdir)
