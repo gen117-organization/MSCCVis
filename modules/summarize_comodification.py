0c0f34f698b60687d5b358aa79068f7afb7e0eb6
YukiOhta0519 <ohtayuki0519@gmail.com>
2025-09-24 01:40:25 +0900

    同時修正があるプロジェクト数を表示してみる．

--- modules/summarize_comodification.py
+++ modules/summarize_comodification.py
@@ -12,6 +12,18 @@ if __name__ == "__main__":
     dataset_file = project_root / "dataset/selected_projects.json"
     with open(dataset_file, "r") as f:
         dataset = json.load(f)
+    results = {}
     for project in dataset:
         comodification_rate = modules.calculate_comodification_rate.analyze_repo(project)
-        print(comodification_rate)
+        for language in comodification_rate:
+            if language not in results:
+                results[language] = {
+                    "within-testing": 0,
+                    "within-production": 0,
+                    "across-testing": 0,
+                    "across-production": 0
+                }
+            for mode in comodification_rate[language]:
+                if comodification_rate[language][mode]["comodification_count"] > 0:
+                    results[language][mode] += 1
+    print(results)
