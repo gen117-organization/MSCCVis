29e5de80655d1d7fcd4dcbae166186119ff7f4b2
YukiOhta0519 <ohtayuki0519@gmail.com>
2025-10-08 14:32:40 +0900

    誤字があった．

--- commands/determine_analyzed_commits.py
+++ commands/determine_analyzed_commits.py
@@ -4,7 +4,7 @@ import json
 
 import git
 
-project_root = Path(__file__).parent.parentj
+project_root = Path(__file__).parent.parent
 sys.path.append(str(project_root))
 
 from config import SELECTED_DATASET, ANALYSIS_FREQUENCY, SEARCH_DEPTH
