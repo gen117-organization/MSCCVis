863f28ae62e7936e2e3778064adedd6ff3c83fc7
YukiOhta0519 <ohtayuki0519@gmail.com>
2025-10-09 11:21:23 +0900

    コレクトデータの実行

--- test/all-test.py
+++ test/all-test.py
@@ -3,17 +3,14 @@ import sys
 from pathlib import Path
 
 project_root = Path(__file__).parent.parent
-
 sys.path.append(str(project_root))
-import modules.clone_repo
-import commands.identify_microservice
+
 import modules.collect_datas
-import modules.analyze_cc
-import modules.analyze_modification
+from config import SELECTED_DATASET
 
 
 if __name__ == "__main__":
-    dataset_file = project_root / "dataset/selected_projects.json"
+    dataset_file = SELECTED_DATASET
     with open(dataset_file, "r") as f:
         dataset = json.load(f)
     outputs = []
@@ -21,8 +18,8 @@ if __name__ == "__main__":
         url = project["URL"]
         name = url.split("/")[-2] + "." + url.split("/")[-1]
         workdir = project_root / "dest/projects" / name
-        modules.clone_repo.clone_repo(url)
+        #modules.clone_repo.clone_repo(url)
         #modules.identify_microservice.analyze_repo(url, name, str(workdir))
         modules.collect_datas.collect_datas_of_repo(project)
-        modules.analyze_cc.analyze_repo(project)
-        modules.analyze_modification.analyze_repo(project)
+        #modules.analyze_cc.analyze_repo(project)
+        #modules.analyze_modification.analyze_repo(project)
